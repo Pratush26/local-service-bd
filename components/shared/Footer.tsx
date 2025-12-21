22e11627b303aafa0a7167f5d876add54d28e3b8
Pratush <pratushbarua26@gmail.com>
2025-12-21 22:19:17 +0600

    starter templete

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <section>
+            <footer>
+                <div>
+                    <p>Footer</p>
+                </div>
+            </footer>
+        </section>
+    )
+}
